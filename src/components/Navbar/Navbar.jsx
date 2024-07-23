db56e7a5097c6bb9478dfdff82158f6520547154
William Espinoza <espinozaw657@gmail.com>
2024-07-23 09:26:42 -0500

    Update Portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
+  return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="/">
-            Portfolio
-        </a>
-        <div className={styles.menu}>
-            <img
-                className={styles.menuBtn} 
-                src={
-                    menuOpen
-                        ? getImageUrl("nav/closeIcon.png")
-                        : getImageUrl("nav/menuIcon.png")
-            }
-            alt="menu-button"
-            onClick={() => setMenuOpen(!menuOpen)}
-            />
-            <ul 
-                className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                onClick={() => setMenuOpen(true)}
-            >
-                <li>
-                    <a href="#about">Sobre mi</a>
-                </li>
-                <li>
-                    <a href="#experience">Experiencia</a>
-                </li>
-                <li>
-                    <a href="#proyects">Proyectos</a>
-                </li>
-                <li>
-                    <a href="#contact">Contacto</a>
-                </li>
-            </ul>
-        </div>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(true)}
+        >
+          <li>
+            <a href="#about">Sobre mi</a>
+          </li>
+          <li>
+            <a href="#experience">Experiencia</a>
+          </li>
+          <li>
+            <a href="#proyects">Proyectos</a>
+          </li>
+          <li>
+            <a href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-    );
+  );
 };
-
